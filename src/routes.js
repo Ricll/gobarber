d9e54a47893bde96b41ff51762f823930a6f7ab6
Ricll <ricllima@hotmail.com>
2019-05-05 21:32:10 -0300

    Agendamento

--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,8 @@ const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const DashBoardController = require('./app/controllers/DashBoardController')
 const FileController = require('./app/controllers/FileController')
+const AppointmentController = require('./app/controllers/AppointmentController')
+const AvailableController = require('./app/controllers/AvailableController')
 
 routes.use((req, res, next) => {
   res.locals.flashSucces = req.flash('success')
@@ -34,5 +36,9 @@ routes.use('/app', authMiddleware)
 routes.get('/app/logout', SessionController.destroy)
 
 routes.get('/app/dashboard', DashBoardController.index)
+
+routes.get('/app/appointments/new/:provider', AppointmentController.create)
+routes.post('/app/appointments/new/:provider', AppointmentController.store)
+routes.get('/app/available/:provider', AvailableController.index)
 
 module.exports = routes
